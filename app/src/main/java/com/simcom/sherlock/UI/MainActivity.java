b65777036669a12e0067bb75146e162be62c0e20
Šimon Vyhnis <simon.vyhnis@gmail.com>
2020-11-20 19:03:44 +0100

    create foreground service

--- app/src/main/java/com/simcom/sherlock/UI/MainActivity.java
+++ app/src/main/java/com/simcom/sherlock/UI/MainActivity.java
@@ -6,21 +6,40 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.simcom.sherlock.R;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int ACTION_NAVIGATE_TO_SHARE_FRAGMENT = 1;
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         LoginViewModel viewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(this.getApplication())).get(LoginViewModel.class);
+        if(getIntent()!=null){
+            navigateToShareFragmentIfNeeded(getIntent());
+        }
         if(viewModel.isLoggedIn()){
                navController.navigate(R.id.action_registerFragment_to_startFragment);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        navigateToShareFragmentIfNeeded(intent);
+    }
+
+    private void navigateToShareFragmentIfNeeded(Intent intent){
+        if(intent.getIntExtra("action",0) == ACTION_NAVIGATE_TO_SHARE_FRAGMENT){
+            navController.navigate(R.id.action_global_shareFragment);
         }
     }
 }
--- app/src/main/java/com/simcom/sherlock/UI/ShareFragment.java
+++ app/src/main/java/com/simcom/sherlock/UI/ShareFragment.java
@@ -11,14 +11,70 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.google.android.gms.maps.MapView;
 import com.simcom.sherlock.R;
 
 public class ShareFragment extends Fragment {
+    private MapView mapView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_share, container, false);
         final NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+        mapView = root.findViewById(R.id.map);
+        root.findViewById(R.id.button_share).setOnClickListener(view -> {
+
+        });
         return root;
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mapView!=null) {
+            mapView.onDestroy();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
